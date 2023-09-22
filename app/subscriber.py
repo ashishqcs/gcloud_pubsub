a977db0eba991beebee631f3c36e337da9091060
Ashish Singh <ashish.q@outlook.com>
2023-09-22 22:27:07 +0530

    add dockerfile

--- app/subscriber.py
+++ app/subscriber.py
@@ -1,10 +1,13 @@
 import base64
 import json
+import logging
 
 from fastapi import APIRouter, Request
 from google.cloud.pubsub_v1.subscriber.message import Message
 
+
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.post("/subscribe")
@@ -13,5 +16,5 @@ async def subscriber(request: Request):
     envelope: Message = json.loads(data)
     message = envelope["message"]["data"]
     payload = base64.b64decode(message).decode()
-    print(f'Received event: {payload}')
-    # logging.info(f'Received message: {message}')
+    # print(f'Received event: {payload}')
+    logger.info(f"Received message: {payload}")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 from app.publisher import router
 from app.subscriber import router as sub_router
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -10,5 +14,5 @@ app.include_router(sub_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080, access_log=True, log_config='log_conf.yml')
 
